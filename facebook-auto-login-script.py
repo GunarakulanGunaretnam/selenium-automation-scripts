b81b50615c5187aaca41b2f075550243ed617459
GunaRakulan-GR <gunarakulan@gmail.com>
2022-03-25 01:21:47 +0530

    Rename script file, Finish facebook

--- facebook-auto-login-script.py
+++ facebook-auto-login-script.py
@@ -2,11 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('./chromedriver')
-driver.get("https://www.python.org")
+driver.get("https://www.google.com")
 print(driver.title)
 search_bar = driver.find_element_by_name("q")
 search_bar.clear()
-search_bar.send_keys("getting started with python")
+search_bar.send_keys("facebook")
 search_bar.send_keys(Keys.RETURN)
-print(driver.current_url)
-driver.close()
+driver.find_element_by_xpath('//*[@id="rso"]/div[1]/div/div/div/div/div/div[1]/a/h3').click()
+search_bar = driver.find_element_by_id("testing_email")
+
+
+
