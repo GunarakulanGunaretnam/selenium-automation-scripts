74a870dae37abc2bb075b55884f90df389d4e56e
GunaRakulan-GR <gunarakulan@gmail.com>
2022-04-03 17:14:17 +0530

    Finish the basic prototype

--- 1-saucedemo-testing-script.py
+++ 1-saucedemo-testing-script.py
@@ -14,9 +14,22 @@ driver.find_element_by_id("login-button").click()
 lenth_of_item = int(len(driver.find_elements_by_css_selector(".btn_inventory")))
 
 for x in range(lenth_of_item):
-	print(x)
-	#driver.find_elements_by_css_selector(".btn_inventory")[x].click()
+	driver.find_elements_by_css_selector(".btn_inventory")[x].click()
 
+
+driver.find_element_by_id("shopping_cart_container").click()
+driver.find_element_by_id("checkout").click()
+
+
+driver.find_element_by_id("first-name").send_keys("Gunarakulan")
+driver.find_element_by_id("last-name").send_keys("Gunaretnam")
+driver.find_element_by_id("postal-code").send_keys("3456754321345")
+
+driver.find_element_by_id("continue").click()
+
+driver.find_element_by_id("finish").click()
+
+driver.find_element_by_id("back-to-products").click()
 
 
 
