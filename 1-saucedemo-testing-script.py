52141b396b6bed6b27b38a0fe8ed7866192d8427
GunaRakulan-GR <gunarakulan@gmail.com>
2022-04-03 16:51:42 +0530

    Add login function

--- 1-saucedemo-testing-script.py
+++ 1-saucedemo-testing-script.py
@@ -6,3 +6,7 @@ driver = webdriver.Chrome("./chromedriver")
 driver.maximize_window()
 
 driver.get("https://www.saucedemo.com/")
+
+driver.find_element_by_id("user-name").send_keys("standard_user")
+driver.find_element_by_id("password").send_keys("secret_sauce")
+driver.find_element_by_id("login-button").click()
