55cd5b0cfe20a409c22a4a6b08dcda784575f03d
GunaRakulan-GR <gunarakulan@gmail.com>
2022-04-03 17:07:31 +0530

    Add a function to click add to card buttons

--- 1-saucedemo-testing-script.py
+++ 1-saucedemo-testing-script.py
@@ -10,3 +10,13 @@ driver.get("https://www.saucedemo.com/")
 driver.find_element_by_id("user-name").send_keys("standard_user")
 driver.find_element_by_id("password").send_keys("secret_sauce")
 driver.find_element_by_id("login-button").click()
+
+lenth_of_item = int(len(driver.find_elements_by_css_selector(".btn_inventory")))
+
+for x in range(lenth_of_item):
+	print(x)
+	#driver.find_elements_by_css_selector(".btn_inventory")[x].click()
+
+
+
+
