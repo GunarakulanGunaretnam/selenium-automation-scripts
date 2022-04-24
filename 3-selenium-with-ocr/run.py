e0989ef80296adbb40e63a51d7c7b2452aef1566
GunaRakulan-GR <gunarakulan@gmail.com>
2022-04-24 20:49:43 +0530

    Fix folder

--- 3-selenium-with-ocr/run.py
+++ 3-selenium-with-ocr/run.py
@@ -4,47 +4,45 @@ from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-
 driver = webdriver.Chrome("./chromedriver")
 driver.maximize_window()
-driver.get("file:///C:/Users/Gunarakulan/Desktop/selenium-with-ocr/index.html")
-
-
+driver.get("file:///C:/Users/Gunarakulan/Desktop/selenium-tutorial-python/3-selenium-with-ocr/index.html")
 
 def clean_up(text):
-    text = text.replace(" ","").replace("01)", "").replace("02)", "").replace("03)", "").replace("04)", "").replace("\n\x0c", "").replace("\n","|")
-    return text
+	text = text.replace(" ", "").replace("01)","").replace("02)","").replace("03)","").replace("04)","").replace("\n\x0c", "").replace("\n", "|")
+	return text
 
-for filename in os.listdir('inputs/'):
+for filename in os.listdir("inputs/"):
+	
+	text = pytesseract.image_to_string(Image.open(f'inputs/{filename}'))
 
-    text = pytesseract.image_to_string(Image.open(f'inputs/{filename}'))
+	cleaned_text = clean_up(text)
 
-    cleaned_data = clean_up(text)
-
-    splited_data = cleaned_data.split("|")
+	splited_text = cleaned_text.split("|")
 
 
-    for x in splited_data:
+	for x in splited_text:
 
-        if x != "":
-            entity_name = x.split("=")[0].strip()
-            entity_value = x.split("=")[1].strip()
+		if x != "":
+			x_splited_data = x.split("=")
+			entity_name = x_splited_data[0].strip()
+			entity_value = x_splited_data[1].strip()
+			
 
-        
+			if entity_name == "FirstName":
+				driver.find_element_by_id("firstname").send_keys(entity_value)
 
-            if entity_name == "FirstName":
-                driver.find_element_by_id("firstname").send_keys(entity_value)
+			elif entity_name == "LastName":
+				driver.find_element_by_id("lastname").send_keys(entity_value)
 
-            elif entity_name == "LastName":
-                driver.find_element_by_id("lastname").send_keys(entity_value)
+			elif entity_name == "Age":
+				driver.find_element_by_id("age").send_keys(entity_value)
 
-            elif entity_name == "Age":
-                driver.find_element_by_id("age").send_keys(entity_value)
+			elif entity_name == "Designation":
+				driver.find_element_by_id("designation").send_keys(entity_value)
 
-            elif entity_name == "Designation":
-                driver.find_element_by_id("designation").send_keys(entity_value)
+	driver.find_element_by_id("btn").click()
 
-    driver.find_element_by_id("btn").click()
 
 
 
